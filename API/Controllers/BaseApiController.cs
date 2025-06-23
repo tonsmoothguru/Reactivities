1a9aacdb479756c1e84de57e76114ab3944b06f9
amanigos@tonsmoothguru.com <anthony.manigos@3cloudsolutions.com>
2025-06-24 01:33:40 +0800

    End of Section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
